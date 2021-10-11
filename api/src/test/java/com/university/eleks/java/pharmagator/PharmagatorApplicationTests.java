93fe15906691cb1052444dedad430d4fb08599e2
Yurii Shalenyk <shalenik.jurij@gmail.com>
2021-10-11 15:26:22 +0200

    #003 Initializing migration db

--- api/src/test/java/com/university/eleks/java/pharmagator/PharmagatorApplicationTests.java
+++ api/src/test/java/com/university/eleks/java/pharmagator/PharmagatorApplicationTests.java
@@ -3,11 +3,14 @@ package com.university.eleks.java.pharmagator;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class PharmagatorApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertEquals(1,9);
 	}
 
 }
