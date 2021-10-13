82c8808dc09f0a5cba5835ef36610d936a33c03a
Yurii Shalenyk <shalenik.jurij@gmail.com>
2021-10-13 14:52:39 +0200

    #003HW Recreation tests

--- api/src/test/java/com/university/eleks/java/pharmagator/PharmagatorApplicationTests.java
+++ api/src/test/java/com/university/eleks/java/pharmagator/PharmagatorApplicationTests.java
@@ -2,8 +2,10 @@ package com.university.eleks.java.pharmagator;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PharmagatorApplicationTests {
 
 	@Test
